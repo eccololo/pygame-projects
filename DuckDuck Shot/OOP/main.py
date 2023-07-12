fe3dc2ca9674d010f46b3cb7e3a047e1c282db80
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-07-12 16:24:55 +0200

    Add animation of surface feature to class surface and to main.

--- DuckDuck Shot/OOP/main.py
+++ DuckDuck Shot/OOP/main.py
@@ -7,10 +7,15 @@ screen = Screen()
 
 # ============= Surface Assets ===============
 wood_bg = Surface("./assets/images/Wood_BG.png", screen)
+
 land_bg = Surface("./assets/images/Land_BG.png", screen)
+land_bg.make_animable_y(480, 500, 460, 0.3)
 water_bg = Surface("./assets/images/Water_BG.png", screen)
+water_bg.make_animable_y(420, 450, 390, 0.6)
+
 cloud_1 = Surface("./assets/images/Cloud1.png", screen)
 cloud_2 = Surface("./assets/images/Cloud2.png", screen)
+
 
 while True:
 
@@ -19,6 +24,8 @@ while True:
             screen.quit_game()
 
     wood_bg.display_surface(0, 0)
+    water_bg.animate_y(0)
+    land_bg.animate_y(0)
 
     pygame.display.update()
     screen.set_clock(120)
--- DuckDuck Shot/OOP/surface.py
+++ DuckDuck Shot/OOP/surface.py
@@ -7,5 +7,23 @@ class Surface:
         self.surface = pygame.image.load(img_path)
         self.screen = screen
 
+        # For animation
+        self.animation_start_y = None
+        self.animation_max_y = None
+        self.animation_min_y = None
+        self.animation_speed = None
+
     def display_surface(self, x_cor, y_cor):
         self.screen.get_screen().blit(self.surface, (x_cor, y_cor))
+
+    def make_animable_y(self, animation_start_y=None, animation_max_y=None, animation_min_y=None, animation_speed=None):
+        self.animation_start_y = animation_start_y
+        self.animation_max_y = animation_max_y
+        self.animation_min_y = animation_min_y
+        self.animation_speed = animation_speed
+
+    def animate_y(self, x_cor):
+        self.animation_start_y += self.animation_speed
+        if self.animation_start_y >= self.animation_max_y or self.animation_start_y <= self.animation_min_y:
+            self.animation_speed *= -1
+        self.screen.get_screen().blit(self.surface, (x_cor, self.animation_start_y))
