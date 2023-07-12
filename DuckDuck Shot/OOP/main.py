9d5cbba45b91ad5fdffddef891139919a67cac56
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-07-12 16:38:52 +0200

    Add two animated in x cor clouds to game window.

--- DuckDuck Shot/OOP/main.py
+++ DuckDuck Shot/OOP/main.py
@@ -14,7 +14,9 @@ water_bg = Surface("./assets/images/Water_BG.png", screen)
 water_bg.make_animable_y(420, 450, 390, 0.6)
 
 cloud_1 = Surface("./assets/images/Cloud1.png", screen)
+cloud_1.make_animable_x(100, 112, 97, 0.2)
 cloud_2 = Surface("./assets/images/Cloud2.png", screen)
+cloud_2.make_animable_x(500, 510, 490, 0.2)
 
 
 while True:
@@ -26,6 +28,8 @@ while True:
     wood_bg.display_surface(0, 0)
     water_bg.animate_y(0)
     land_bg.animate_y(0)
+    cloud_1.animate_x(60)
+    cloud_2.animate_x(120)
 
     pygame.display.update()
     screen.set_clock(120)
--- DuckDuck Shot/OOP/surface.py
+++ DuckDuck Shot/OOP/surface.py
@@ -11,37 +11,37 @@ class Surface:
         self.animation_start_y = None
         self.animation_max_y = None
         self.animation_min_y = None
-        self.animation_speed = None
+        self.animation_speed_y = None
 
         # For animation x
         self.animation_start_x = None
         self.animation_max_x = None
         self.animation_min_x = None
-        self.animation_speed = None
+        self.animation_speed_x = None
 
     def display_surface(self, x_cor, y_cor):
         self.screen.get_screen().blit(self.surface, (x_cor, y_cor))
 
-    def make_animable_y(self, animation_start_y=None, animation_max_y=None, animation_min_y=None, animation_speed=None):
+    def make_animable_y(self, animation_start_y, animation_max_y, animation_min_y, animation_speed_y):
         self.animation_start_y = animation_start_y
         self.animation_max_y = animation_max_y
         self.animation_min_y = animation_min_y
-        self.animation_speed = animation_speed
+        self.animation_speed_y = animation_speed_y
 
-    def make_animable_x(self, animation_start_x=None, animation_max_x=None, animation_min_x=None, animation_speed=None):
+    def make_animable_x(self, animation_start_x, animation_max_x, animation_min_x, animation_speed_x):
         self.animation_start_x = animation_start_x
         self.animation_max_x = animation_max_x
         self.animation_min_x = animation_min_x
-        self.animation_speed = animation_speed
+        self.animation_speed_x = animation_speed_x
 
     def animate_y(self, x_cor):
-        self.animation_start_y += self.animation_speed
+        self.animation_start_y += self.animation_speed_y
         if self.animation_start_y >= self.animation_max_y or self.animation_start_y <= self.animation_min_y:
-            self.animation_speed *= -1
+            self.animation_speed_y *= -1
         self.screen.get_screen().blit(self.surface, (x_cor, self.animation_start_y))
 
     def animate_x(self, y_cor):
-        self.animation_start_x += self.animation_speed
+        self.animation_start_x += self.animation_speed_x
         if self.animation_start_x >= self.animation_max_x or self.animation_start_x <= self.animation_min_x:
-            self.animation_speed *= -1
-        self.screen.get_screen().blit(self.surface, (y_cor, self.animation_start_x))
+            self.animation_speed_x *= -1
+        self.screen.get_screen().blit(self.surface, (self.animation_start_x, y_cor))
