bf3b5b9e4e9dae9c132c00bf4e8c6dde36363476
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-01-09 09:44:17 +0100

    Exporting all game constants to settings.py file.

--- DuckDuck Shot/standard/main.py
+++ DuckDuck Shot/standard/main.py
@@ -1,19 +1,7 @@
 import pygame
 import sys
 import random
-
-SCREEN_WIDTH = 1020
-SCREEN_HEIGHT = 600
-NUM_OF_DUCKS = 3
-MAX_NUM_OF_DUCKS = 15
-NUM_OF_CLOUDS = 9
-GAME_LEVEL = 1
-MAX_GAME_LVL = 15
-GAME_LEVEL_START_TIME = 3
-GAME_LEVEL_UPDATE_TIME = GAME_LEVEL_START_TIME
-IS_NEXT_LVL = False
-IS_GAME_OVER = False
-GAME_WON = False
+from settings import *
 
 
 def gen_ducks(game_lvl):
